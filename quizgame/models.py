8c5240e7b0ac61e297951f2573023b71c31fd578
김종혁 <hwaeen@gmail.com>
2019-07-24 15:54:28 +0900

    초기화면 template 상속 작성

--- quizgame/models.py
+++ quizgame/models.py
@@ -1,21 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Quizmaker():
-    username = models.CharFields(max_length=20)
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-
-class Quizsolver():
-    username = models.CharField(max_length=20)
-
-class Answer():
-    first = models.CharField(max_length=1)
-    second = models.CharField(max_length=1)
-    third = models.CharField(max_length=1)
-    fourth = models.CharField(max_length=1)
-    fifth = models.CharField(max_length=1)
-    sixth = models.CharField(max_length=1)
-    seventh = models.CharField(max_length=1)
-    eighth = models.CharField(max_length=1)
-    nineth = models.CharField(max_length=1)
-    tenth = models.CharField(max_length=1)
--- quizgame/views.py
+++ quizgame/views.py
@@ -1,32 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
-from .models import Quizmaker, Quizsolver
+from django.shortcuts import render
 
 
-
-def quizmaker_create(request):
-    if request.method == 'POST':
-        print(request.POST)
-        user = Quizmaker()
-        user.username = request.POST['name']
-
-        user.save()
-        return redirect(reverse('#', kwargs={'pk': user.id}))
-
-    elif request.method == 'GET':
-        return render(request, '#')
-
-def quizmaker_update(request, pk):
-    if request.method == 'POST':
-        user = get_object_or_404(Quizmaker, pk=pk)
-        user.count = request.POST.get('choice.id') ==
-        user.contents = request.POST['contents']
-        user.author = request.POST['author']
-        user.save()
-        return redirect(reverse('core:article_detail', kwargs={'pk': article.pk}))
-    elif request.method == 'GET':
-        article = get_object_or_404(Article, pk=pk)
-        return render(request, 'core/article_create.html', {"article" : article})
+def quizstart(request):
+    return render(request, 'quizgame/quizmaker_main.html')
